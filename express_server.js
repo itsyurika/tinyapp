e37ba6da9195f7b80cc875615a5ac1044a454805
Yuri_Windows_Desktop <07yuri@gmail.com>
2022-04-18 12:55:33 -0400

    adding urls index page with EJS

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -14,17 +15,18 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: 'Hello World!' };
-  res.render("hello_world", templateVars);
-});
+// app.get("/hello", (req, res) => {
+//   const templateVars = { greeting: 'Hello World!' };
+//   res.render("hello_world", templateVars);
+// });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
