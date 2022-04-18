afbbf1e71d669058d85c78abf0728e5925f98dba
Yuri_Windows_Desktop <07yuri@gmail.com>
2022-04-18 14:41:49 -0400

    implementing randomstring generating function, and routing for POST request

--- express_server.js
+++ express_server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, '/views'));
 
@@ -11,22 +14,35 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  // or can use Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substring(0, 6);
+  const randomString = "";
+  const charactersSet = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const setLength = charactersSet.length;
+  for (let i = 0; i < 7; i++) {
+    randomString += characterSet.charAt(Math.floor(Math.random() * setLength));
+  }
+
+  return randomString;
+
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-// app.get("/hello", (req, res) => {
-//   const templateVars = { greeting: 'Hello World!' };
-//   res.render("hello_world", templateVars);
-// });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -36,6 +52,7 @@ app.get("/urls/:shortURL", (req, res) => {
   // const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
